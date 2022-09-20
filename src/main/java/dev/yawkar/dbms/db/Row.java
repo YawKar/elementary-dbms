3a71740e449454c2d5ebf6f5306685d3a16a2813
yawkar <yawkarpov@gmail.com>
2022-09-20 20:11:53 +0300

    feat: add set method to Row & create UpdateSpecification

--- src/main/java/dev/yawkar/dbms/db/Row.java
+++ src/main/java/dev/yawkar/dbms/db/Row.java
@@ -3,4 +3,5 @@ package dev.yawkar.dbms.db;
 public interface Row {
 
     RowElement get(int index);
+    void set(int index, RowElement rowElement);
 }
---
+++ src/main/java/dev/yawkar/dbms/specification/UpdateSpecification.java
@@ -0,0 +1,8 @@
+package dev.yawkar.dbms.specification;
+
+import dev.yawkar.dbms.db.Row;
+
+public interface UpdateSpecification {
+
+    void update(Row row);
+}
