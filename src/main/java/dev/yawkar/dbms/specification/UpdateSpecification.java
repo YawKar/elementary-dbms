ab79f01740c971c9885784581dadbf579d688dce
yawkar <yawkarpov@gmail.com>
2022-09-22 16:35:04 +0300

    feat: add setup() method to UpdateSpecification
    this method used for setting up column index when update should be performed

--- src/main/java/dev/yawkar/dbms/specification/UpdateSpecification.java
+++ src/main/java/dev/yawkar/dbms/specification/UpdateSpecification.java
@@ -1,8 +1,10 @@
 package dev.yawkar.dbms.specification;
 
 import dev.yawkar.dbms.db.Row;
+import dev.yawkar.dbms.db.Table;
 
 public interface UpdateSpecification {
 
     void update(Row row);
+    default void setup(Table table) {}
 }
