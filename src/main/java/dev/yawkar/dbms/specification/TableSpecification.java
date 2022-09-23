a25f8cb5fda7ae7860c47baf4b548d1c4b4e8d1f
yawkar <yawkarpov@gmail.com>
2022-09-23 20:40:27 +0300

    feat: describe TableSpecification

--- src/main/java/dev/yawkar/dbms/specification/TableSpecification.java
+++ src/main/java/dev/yawkar/dbms/specification/TableSpecification.java
@@ -1,4 +1,16 @@
 package dev.yawkar.dbms.specification;
 
+import java.util.List;
+
 public interface TableSpecification {
+
+    String getName();
+    void setName(String name);
+    List<ColumnSpecification> getColumns();
+    ColumnSpecification getColumn(int index);
+    ColumnSpecification getColumn(String columnLabel);
+    void addColumn(ColumnSpecification column);
+    void addColumn(int index, ColumnSpecification column);
+    void remove(int index);
+    void remove(String columnLabel);
 }
