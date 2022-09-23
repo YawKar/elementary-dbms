2f885d9040694a2fe478110d280586fbab0c93df
yawkar <yawkarpov@gmail.com>
2022-09-23 20:33:19 +0300

    feat: describe ColumnSpecification

--- src/main/java/dev/yawkar/dbms/specification/ColumnSpecification.java
+++ src/main/java/dev/yawkar/dbms/specification/ColumnSpecification.java
@@ -1,4 +1,13 @@
 package dev.yawkar.dbms.specification;
 
 public interface ColumnSpecification {
+
+    String getLabel();
+    void setLabel(String label);
+
+    boolean isPK();
+    void setPK(boolean pk);
+
+    String getType();
+    void setType(String type);
 }
